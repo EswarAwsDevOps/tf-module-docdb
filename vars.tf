e9abc3cf7612b09c8f53d635ec906bce45931165
eswar <eswar.awsdevops@gmail.com>
2023-01-06 20:03:16 +0530

    creating DocDb

--- vars.tf
+++ vars.tf
@@ -2,5 +2,5 @@ variable "env" {}
 variable "engine" {}
 variable "backup_retention_period" {}
 variable "preferred_backup_window" {}
-variable "skip_final_snapshot-" {}
+variable "skip_final_snapshot" {}
 variable "vpc" {}
